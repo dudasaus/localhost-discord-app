4a082310340282aeb08a540c7bc7dbbccd9faed4
Austin Dudas <dudas.austin@gmail.com>
2025-03-10 22:10:44 -0600

    use dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import "@std/dotenv/load";
 import { Hono } from "@hono/hono";
 import { DiscordHono } from "@dudasaus/discord-hono";
 
 const app = new Hono();
-const discordApp = new DiscordHono(app);
+const _discordApp = new DiscordHono(app);
 
 app.get("/", (c) => {
   return c.text("Hello world");

6ce137e49f86c6af264352dbf8e732e210050a63
Austin Dudas <dudas.austin@gmail.com>
2025-03-10 23:17:01 -0600

    Add a command registration flag

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import "@std/dotenv/load";
 import { Hono } from "@hono/hono";
 import { DiscordHono } from "@dudasaus/discord-hono";
+import { parseArgs } from "@std/cli/parse-args";
+
+const flags = parseArgs(Deno.args, {
+  boolean: ["register"],
+});
 
 const app = new Hono();
 const discordApp = new DiscordHono(app);
@@ -15,4 +20,24 @@ app.get("/", (c) => {
   return c.json({ status: "ok" });
 });
 
-Deno.serve(app.fetch);
+if (flags.register) {
+  const response = await fetch(
+    `https://discord.com/api/v10/applications/${
+      Deno.env.get("DISCORD_APP_ID")
+    }/commands`,
+    {
+      method: "POST",
+      headers: {
+        "Authorization": `Bot ${Deno.env.get("DISCORD_BOT_TOKEN")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "status",
+        description: "Check the status of the discord bot",
+      }),
+    },
+  );
+  console.log(response.status, await response.json());
+} else {
+  Deno.serve(app.fetch);
+}
